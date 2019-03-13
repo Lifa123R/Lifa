91d187916ba8d0275a5467df1ff63d0dc29565c8
Lifa123R <35253174+Lifa123R@users.noreply.github.com>
2019-03-13 07:33:48 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.txtFullName = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtxPhoneNumber = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtSearch = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnNew = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnCancel = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnEdit = new MaterialSkin.Controls.MaterialFlatButton();
+            this.phoneNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneListBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.myAppData = new WindowsFormsApp1.MyAppData();
+            this.phoneListBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.myAppDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.phoneListTableAdapter = new WindowsFormsApp1.MyAppDataTableAdapters.PhoneListTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myAppData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myAppDataBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(72, 117);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(97, 24);
+            this.materialLabel1.TabIndex = 2;
+            this.materialLabel1.Text = "Full Name";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(70, 155);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(140, 24);
+            this.materialLabel2.TabIndex = 2;
+            this.materialLabel2.Text = "Phone Number";
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(72, 196);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(70, 24);
+            this.materialLabel4.TabIndex = 1;
+            this.materialLabel4.Text = "Search";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneListBindingSource, "FullName", true));
+            this.txtFullName.Depth = 0;
+            this.txtFullName.Hint = "";
+            this.txtFullName.Location = new System.Drawing.Point(212, 117);
+            this.txtFullName.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.PasswordChar = '\0';
+            this.txtFullName.SelectedText = "";
+            this.txtFullName.SelectionLength = 0;
+            this.txtFullName.SelectionStart = 0;
+            this.txtFullName.Size = new System.Drawing.Size(466, 28);
+            this.txtFullName.TabIndex = 0;
+            this.txtFullName.UseSystemPasswordChar = false;
+            // 
+            // txtxPhoneNumber
+            // 
+            this.txtxPhoneNumber.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneListBindingSource, "PhoneNumber", true));
+            this.txtxPhoneNumber.Depth = 0;
+            this.txtxPhoneNumber.Hint = "";
+            this.txtxPhoneNumber.Location = new System.Drawing.Point(212, 155);
+            this.txtxPhoneNumber.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtxPhoneNumber.Name = "txtxPhoneNumber";
+            this.txtxPhoneNumber.PasswordChar = '\0';
+            this.txtxPhoneNumber.SelectedText = "";
+            this.txtxPhoneNumber.SelectionLength = 0;
+            this.txtxPhoneNumber.SelectionStart = 0;
+            this.txtxPhoneNumber.Size = new System.Drawing.Size(466, 28);
+            this.txtxPhoneNumber.TabIndex = 1;
+            this.txtxPhoneNumber.UseSystemPasswordChar = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Depth = 0;
+            this.txtSearch.Hint = "";
+            this.txtSearch.Location = new System.Drawing.Point(212, 192);
+            this.txtSearch.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PasswordChar = '\0';
+            this.txtSearch.SelectedText = "";
+            this.txtSearch.SelectionLength = 0;
+            this.txtSearch.SelectionStart = 0;
+            this.txtSearch.Size = new System.Drawing.Size(807, 28);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.UseSystemPasswordChar = false;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.phoneNumberDataGridViewTextBoxColumn,
+            this.fullNameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.phoneListBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 257);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(992, 209);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            //this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // btnSave
+            // 
+            this.btnSave.AutoSize = true;
+            this.btnSave.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSave.Depth = 0;
+            this.btnSave.Location = new System.Drawing.Point(939, 490);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSave.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Primary = false;
+            this.btnSave.Size = new System.Drawing.Size(56, 36);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.AutoSize = true;
+            this.btnNew.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNew.Depth = 0;
+            this.btnNew.Location = new System.Drawing.Point(681, 490);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnNew.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Primary = false;
+            this.btnNew.Size = new System.Drawing.Size(50, 36);
+            this.btnNew.TabIndex = 3;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCancel.Depth = 0;
+            this.btnCancel.Location = new System.Drawing.Point(835, 490);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnCancel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Primary = false;
+            this.btnCancel.Size = new System.Drawing.Size(78, 36);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.AutoSize = true;
+            this.btnEdit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnEdit.Depth = 0;
+            this.btnEdit.Location = new System.Drawing.Point(763, 490);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnEdit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Primary = false;
+            this.btnEdit.Size = new System.Drawing.Size(49, 36);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // phoneNumberDataGridViewTextBoxColumn
+            // 
+            this.phoneNumberDataGridViewTextBoxColumn.DataPropertyName = "PhoneNumber";
+            this.phoneNumberDataGridViewTextBoxColumn.HeaderText = "PhoneNumber";
+            this.phoneNumberDataGridViewTextBoxColumn.Name = "phoneNumberDataGridViewTextBoxColumn";
+            this.phoneNumberDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // fullNameDataGridViewTextBoxColumn
+            // 
+            this.fullNameDataGridViewTextBoxColumn.DataPropertyName = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.HeaderText = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.Name = "fullNameDataGridViewTextBoxColumn";
+            this.fullNameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // phoneListBindingSource1
+            // 
+            this.phoneListBindingSource1.DataMember = "PhoneList";
+            this.phoneListBindingSource1.DataSource = this.myAppData;
+            // 
+            // myAppData
+            // 
+            this.myAppData.DataSetName = "MyAppData";
+            this.myAppData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // phoneListBindingSource
+            // 
+            this.phoneListBindingSource.DataMember = "PhoneList";
+            this.phoneListBindingSource.DataSource = this.myAppData;
+            // 
+            // myAppDataBindingSource
+            // 
+            this.myAppDataBindingSource.DataSource = this.myAppData;
+            this.myAppDataBindingSource.Position = 0;
+            // 
+            // phoneListTableAdapter
+            // 
+            this.phoneListTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtxPhoneNumber);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telephone List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myAppData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneListBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myAppDataBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtFullName;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtxPhoneNumber;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MaterialSkin.Controls.MaterialFlatButton btnSave;
+        private MaterialSkin.Controls.MaterialFlatButton btnNew;
+        private MaterialSkin.Controls.MaterialFlatButton btnCancel;
+        private MaterialSkin.Controls.MaterialFlatButton btnEdit;
+        private MyAppData myAppData;
+        private System.Windows.Forms.BindingSource myAppDataBindingSource;
+        private System.Windows.Forms.BindingSource phoneListBindingSource;
+        private MyAppDataTableAdapters.PhoneListTableAdapter phoneListTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource phoneListBindingSource1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, 
+                                                     Primary.BlueGrey500, Accent.LightBlue200,
+                                                     TextShade.WHITE);
+        }
+
+        private void Edit (bool value)
+        {
+            txtFullName.Enabled = value;
+            txtxPhoneNumber.Enabled = value;
+
+        }
+
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Edit(false);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Edit(true);
+                myAppData.PhoneList.AddPhoneListRow(myAppData.PhoneList.NewPhoneListRow());
+                phoneListBindingSource.MoveLast();
+                txtxPhoneNumber.Focus();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                myAppData.PhoneList.RejectChanges();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+           
+            Edit(true);
+            txtxPhoneNumber.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Edit(false);
+            phoneListBindingSource.ResetBindings(false);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Edit(false);
+                phoneListBindingSource.EndEdit();
+                phoneListTableAdapter.Update(myAppData.PhoneList);
+                dataGridView1.Refresh();
+                txtxPhoneNumber.Focus();
+                MessageBox.Show("Data saved successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                myAppData.PhoneList.RejectChanges();
+            }
+        }
+
+        //private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    if(e.KeyChar == (char)13)
+        //    {
+        //        if (MessageBox.Show("are you sure want to delete?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+        //            phoneListBindingSource.RemoveCurrent();
+
+        //    }
+        //}
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (!string.IsNullOrEmpty(txtSearch.Text))
+                    phoneListBindingSource.Filter = string.Format("PhoneNumber ='{0}'OR FullName LIKE '*{1}*'", txtxPhoneNumber.Text, txtFullName.Text);
+                else
+                    phoneListBindingSource.Filter = string.Empty;
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+
+                if (MessageBox.Show("are you sure want to delete?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    phoneListBindingSource.RemoveCurrent();
+
+            }
+
+        }
+    }
+}
